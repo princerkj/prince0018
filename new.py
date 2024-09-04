f7fd9338e1d60adfa13bad244da3fe47c6c4c48f
princerkj <princekr99j@gmail.com>
2024-09-04 17:53:20 +0530

    resolve new.py

--- new.py
+++ new.py
@@ -1,43 +1,43 @@
 import random
-class CricketGame:
+class Cricket_Game:
     
-  def_init_(self,teamA,teamB):
+  def __init__(self,teamA,teamB):
     self.teamA = teamA # type: ignore
     self.teamA = teamA
     self.teamA_wins = 0
     self.teamA_wins = 0
 
-  def_stimulate_matches(self): 
+  def _stimulate_matches(self): 
 
-  winner = random.choice(self.teamA, self.teamB)
-  if winner == self.teamA :
-      self.teamA_wins += 1
-  else :
-      self.teamB_wins += 1
-      return winner
+    winner = random.choice(self.teamA, self.teamB)
+    if winner == self.teamA :
+        self.teamA_wins += 1
+    else :
+        self.teamB_wins += 1
+        return winner
 
-  def_stimulate_series(self, num_matches):
+  def _stimulate_series(self, num_matches):
 
-  for _ in range(num_matches)
-    self.stimulate_matches()
+    for _ in range(num_matches):
+      self.stimulate_matches()
 
-  if self.teamA_wins>self.teamB_wins:
-    return {
-          'winner': self.teamA,
-          'matches_won' : self.teamA_wins
-    }
+    if self.teamA_wins>self.teamB_wins:
+      return {
+            'winner': self.teamA,
+            'matches_won' : self.teamA_wins
+      }
 
-  elif :
-    return {
-          'winner': self.teamB,
-          'matches_won': self.teamB_wins
-    }
+    elif self.teamA_wins<self.teamB_wins:
+      return {
+            'winner': self.teamB,
+            'matches_won': self.teamB_wins
+      }
 
-  else:
-    return {
-          'winner': Draw,
-          'matches_won': None
-    }
+    else:
+      return {
+            'winner': "Draw",
+            'matches_won': None
+      }
 
 def get_series_winner(num_matches):
    game = Cricket_Game("TeamA","TeamB")
@@ -46,4 +46,4 @@ def get_series_winner(num_matches):
 
 num_matches = 9
 series_result = get_series_winner(num_matches)
-print ("series_winner": {series_winner['winner']}, matches_won= {series_winner['matches_won']} )
+print("series_winner": {series_winner['winner']}, matches_won= {series_winner['matches_won']})
